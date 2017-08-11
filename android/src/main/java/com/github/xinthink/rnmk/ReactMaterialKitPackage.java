7181228e51dc95d2de2238cea2178346148dd0fe
Daniel Landers <daniel.landers@outlook.com>
2017-08-11 13:06:40 +1200

    remove deprecated override for React 0.47.1

--- android/src/main/java/com/github/xinthink/rnmk/ReactMaterialKitPackage.java
+++ android/src/main/java/com/github/xinthink/rnmk/ReactMaterialKitPackage.java
@@ -22,12 +22,12 @@ public class ReactMaterialKitPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    // Deprecated RN 0.47
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
         return Arrays.<ViewManager>asList(
                 new MKTouchableManager(),
